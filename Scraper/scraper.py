c078d32062e897f4d4fe20c47e7c5269567c069f
tzingle1 <trent.zingleman@wsu.edu>
2019-04-16 00:34:33 -0700

    added capability for scraper.py to read from archived tweets (future implementation will include real-time tweets)

--- Scraper/scraper.py
+++ Scraper/scraper.py
@@ -6,7 +6,24 @@ import re
 import requests
 from tqdm import tqdm
 from datetime import datetime
+import calendar
+import time
 from math import ceil
+import json
+
+def createFilename(filename):
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    alphabetPos = 0
+    isFirst = True
+    while os.path.isfile('../Data/Sorted/' + filename + '.txt'):
+        #the file already exists, so lets add a letter to keep it in order
+        if isFirst:     #this is the first copy of the file
+            filename += alphabet[alphabetPos]
+            isFirst = False
+        else:       #this is not the first copy of the file so iterate down the alphabet to keep it in order
+            filename = filename[:-1] + alphabet[alphabetPos]
+        alphabetPos += 1
+    return '../Data/Sorted/' + filename + '.txt'
 
 #retreive all links of Trump's public speeches
 def getLinks():
@@ -25,9 +42,6 @@ def getLinks():
         htmlData = htmlData.replace('<a href=\"/people/president/donald-j-trump\">Donald J. Trump</a>','')      #remove all instances of this hyperlink so the only ones left are the ones we want
         links += [link for link in re.findall(r'(?<=<a href=")[^"]*', htmlData) if 'notice' not in link and 'joint-statement' not in link]            #now extract all links to Trump's public speeches and add them to the list, excluding notices
     links.append('/documents/address-before-joint-session-the-congress-the-state-the-union-26')     #adding this too, it didnt get found in the search query apparently
-    with codecs.open('links.txt', 'w', encoding='utf8') as outfile:
-        for link in links:
-            outfile.write('%s\n' %(link))
     return links
 
 
@@ -38,7 +52,10 @@ def scrape(links):
     htmlCleaner = re.compile(r'(<(([a-z]{1,6}[^>]+)|(/span)|([/]?p))> *)|( *[\(\[].*?[\)\]] *)')
     for i, link in tqdm(enumerate(links), desc="Scraping Data From Links", total=len(links)):   
         htmlData = requests.get(baseUrl + link).text
-        with codecs.open(link.replace('/documents/', '') +'.txt', 'w', encoding='utf8') as outfile:     
+        articleTime = re.findall(r'(?<=<span class="date-display-single" property="dc:date" datatype="xsd:dateTime" content=")[^+]*', htmlData)[0]  #get time of article
+        epochArticleTime = calendar.timegm(time.strptime(articleTime, '%Y-%m-%dT%H:%M:%S'))     #convert article time to time since epoch
+        filename = createFilename(str(epochArticleTime))
+        with codecs.open(filename, 'w', encoding='utf8') as outfile: 
             htmlData = filterToArticle.search(htmlData).groups()[0]     #we are grabbing the text of the article itself
             htmlData = htmlCleaner.sub('', htmlData)        #this cleans out HTML tags that we dont want
             
@@ -58,17 +75,49 @@ def scrape(links):
                 htmlData = re.sub(r'<[/]?[a-z]{1,6}>', '', htmlData)
             
             htmlData = re.sub(r'(\n)|(\s{2,})', ' ', htmlData)       #remove the spaces
-            outfile.write('%s' %(htmlData))
+            outfile.write(' %s ' %(htmlData))
 
 
-def combine():
-    with codecs.open('scrape_full.txt', 'wb', encoding='utf8') as outfile:
-        for filename in glob.glob('*.txt'):
+def readTweets():
+    with open('../Data/tweets/trumpTweets.JSON') as infile:
+        data = json.load(infile)
+        for tweet in tqdm(data["tweets"], desc="Reading Donald Trump's Tweets"):
+            epochTweetTime = calendar.timegm(time.strptime(tweet["created_at"], '%a %b %d %H:%M:%S %z %Y'))     #convert article time to time since epoch
+            filename = createFilename(str(epochTweetTime))
+            with codecs.open(filename, 'w', encoding='utf8') as outfile:
+                outfile.write(' %s ' %(re.sub(r"http\S+", "", tweet["text"])))
+
+
+def combineAll():
+    with codecs.open('../Data/tempfile.txt', 'wb', encoding='utf8') as outfile:
+        with codecs.open('../Data/beforePresidency/trump_pre_election_speeches.txt', 'rb', encoding='utf8') as firstfile:
+            shutil.copyfileobj(firstfile, outfile)
+        for filename in tqdm(glob.glob('../Data/Sorted/*.txt'), desc='Combining Data In Order'):
             with codecs.open(filename, 'rb', encoding='utf8') as readfile:
-                if filename != 'scrape_full.txt':
-                    shutil.copyfileobj(readfile, outfile)
+                shutil.copyfileobj(readfile, outfile)
+
+    with codecs.open('../Data/trump_data_full.txt', 'wb', encoding='utf8') as outfile:
+        with codecs.open('../Data/tempfile.txt', 'rb', encoding='utf8') as infile:
+            data = infile.read()
+            data = re.sub(r'(AUDIENCE MEMBER:).*(MR. TRUMP:)', '', data)
+            data = re.sub(r'([ ]*--[ ]*){2,}', ' -- ', data)
+            data = re.sub(r'&amp;', '', data)
+            data = re.sub(r'[? ]{2,}', ' ', data)
+            data = re.sub(r'[. ]{3,}', '... ', data)
+            data = re.sub(r'(\n)|(\s{2,})', ' ', data)
+            outfile.write('%s' %(data))
+
+def cleanUp():
+    dataFiles = glob.glob('../Data/Sorted/*')
+    for file in tqdm(dataFiles, desc='Cleaning Up'):
+        os.remove(file)
+    os.remove('../Data/tempfile.txt')
+    os.rmdir('../Data/Sorted')
 
 if __name__ == "__main__":
+    os.makedirs("../Data/Sorted")
     links = getLinks()
     scrape(links)
-    combine()
+    readTweets()
+    combineAll()
+    cleanUp()
